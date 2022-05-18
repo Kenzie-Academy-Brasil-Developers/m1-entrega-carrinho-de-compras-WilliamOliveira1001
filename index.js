93c83d9faf6ff3c3cb8e2c2289ba35bc7aafba8f
User <willaoliveirasouza@gmail.com>
2022-05-17 23:38:22 -0300

    implementando minha funcao de soma

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const productsCart = [
-    {
-        id: 1,
-        name: "",
-        price: 0.00
+const listaProdutoPreco = document.querySelector(".listaProdutos")
+const resultadoDaCompra = document.querySelector(".resultado")
+
+
+function criarProdutos(produtos){
+    const nome  = produtos.name
+    const valor = produtos.price
+ 
+    const li       = document.createElement("li")
+    const tagNome  = document.createElement("p")
+    const tagPreco = document.createElement("p")
+
+    tagNome.innerText  = nome
+    tagPreco.innerText = valor
+
+    li.appendChild(tagNome)
+    li.appendChild(tagPreco)
+
+    return li
+}
+
+
+let soma = 0
+function listarItens (listarItens){
+    for(let i=0 ; i<listarItens.length ; i++){
+        const itens = listarItens[i]
+        const cardItens = criarProdutos(itens) //chamando minha funcao e passando minha array
+        listaProdutoPreco.appendChild(cardItens) // colocando meu li dentro do meu ul e exibindo 
+        soma += listarItens[i].price
     }
-]
+}
+listarItens(dataBase)
+
+
+const valor = document.createElement("p")
+const somaTotal = soma
+valor.innerText = somaTotal
+resultadoDaCompra.appendChild(valor)
+
+
---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+const dataBase = [{
+        name: "Celular",
+        price: 100,
+    },
+    {
+        name: "Televisao",
+        price: 200,
+    },
+    {
+        name: "maquinha ",
+        price: 70,
+    },
+    {
+        name: "maquinha ",
+        price: 70,
+    },
+    {
+        name: "maquinha ",
+        price: 70,
+    },
+    {
+        name: "maquinha ",
+        price: 70,
+    },
+    
+]
